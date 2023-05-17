3c26d7ad954e50cc839bbc429a0ea702367e0b79
gmilanese <gmilanese@gmail.com>
2023-05-17 12:33:24 -0400

    agrego async await and try/catch/finally

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,7 @@
    let carrito = [];
 
    function agregarAlCarrito(nombre, precio) {
-   // Verificar si el carrito ya contiene un item con el mismo nombre
-   const carritoItem = carrito.find(item => item.nombre === nombre);
+   const carritoItem = carrito.find(item => item.nombre === nombre); // Verificar si el carrito ya contiene un item con el mismo nombre
  
    if (carritoItem) {
      // Si ya existe un item con el mismo nombre, actualizar la cantidad y el precio total
@@ -39,7 +38,7 @@
     nuevosProductosHTML += `
       <div>
         <span>${producto.nombre}</span>
-        <span>$${producto.precio}</span>
+        <span class="precio-carrito">$${producto.precio}</span>
       </div>
     `;
   });
@@ -60,43 +59,89 @@
   });
   }
 
-  // Cargar el archivo JSON, convertirlo y guardarlo en data.
-  fetch('../data/productos.json')
-  .then(response => response.json())
-  .then(data => {
-    
-    //tomo el div con el id "products-container"
-    const productsContainer = document.getElementById('products-container');
+//cargo el json y genero el html//
+let data = [];
 
-    //recorro data, y para cada (producto) voy creando un div.
+async function cargarDatos() {
+  try {
+    const response = await fetch('../data/productos.json');
+    const dataJson = await response.json();
+    data = dataJson;
+
+    const productsContainer = document.getElementById('products-container');
+    
+    // Recorrer los datos y crear un div para cada producto
     data.forEach(product => {
       const productDiv = document.createElement('div');
       productDiv.classList.add('producto-card');
-
-      //a este nuevo div, que luego sera hijo, le genero el html:
+  
+      // Generar el HTML para el div del producto
       productDiv.innerHTML = `
         <h2 class="product-card__title">${product.nombre}</h2>
         <img src="${product.imagen}" alt="${product.nombre}" class="product-card__image">
         <p class="product-card__price">Price: $${product.precio}</p>
         <button id="btn-${product.id}" class="product-card__button">Add to Cart</button>
       `;
-      
-      // hago un query, buscando el boton (btn-1, por ejemplo).
+  
       const btnAgregarAlCarrito = productDiv.querySelector(`#btn-${product.id}`);
-      
-      //al boton le agrego un listener
+  
+      // Agregar un listener al botón para agregar al carrito
       btnAgregarAlCarrito.addEventListener('click', () => {
         agregarAlCarrito(product.nombre, product.precio);
       });
-
-      //le agrego el hijo al div padre usando el metodo appendchild.
+  
+      // Agregar el div del producto como hijo del contenedor de productos
       productsContainer.appendChild(productDiv);
     });
-  })
+  } catch (error) {
+    console.error(error);
+  } finally {
+    // Código a ejecutar independientemente de si ocurre un error o no. Se que este bloque es opcional, pero lo dejo.
+  }
+}
 
-  .catch(error => console.error(error));
+// Llamar a la función para cargar los datos
+cargarDatos();
   
+//busqueda
+    const searchForm = document.getElementById('search-form');
+    searchForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // para evitar que se envíe el formulario
+        const searchInput = document.getElementById('search-input');
+        const searchTerm = searchInput.value.trim(); // obtener el término de búsqueda
+        if (searchTerm) {
+          search(searchTerm); // llamar a la función de búsqueda con el término de búsqueda
+        }
+    });
+    
+    function search(searchTerm) {
+        const resultados = data.filter(item => item.nombre.toLowerCase().includes(searchTerm.toLowerCase()));
+        
+        const container = document.querySelector('#products-container');
+        container.innerHTML = ''; // Limpiar el contenido anterior del contenedor
   
+        resultados.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('producto-card');
+  
+        productDiv.innerHTML = `
+            <h2 class="product-card__title">${product.nombre}</h2>
+            <img src="${product.imagen}" alt="${product.nombre}" class="product-card__image">
+            <p class="product-card__price">Price: $${product.precio}</p>
+            <button id="btn-${product.id}" class="product-card__button">Add to Cart</button>
+        `;
+
+        // hago un query, buscando el boton (btn-1, por ejemplo).
+        const btnAgregarAlCarrito = productDiv.querySelector(`#btn-${product.id}`);
+      
+        //al boton le agrego un listener
+        btnAgregarAlCarrito.addEventListener('click', () => {
+        agregarAlCarrito(product.nombre, product.precio);
+      });
+      
+        container.appendChild(productDiv);
+    });
+    }
  
 
   
